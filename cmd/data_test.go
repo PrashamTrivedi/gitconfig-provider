0539708132ccdc070545e08884f5e28200cd4f55
PrashamTrivedi <PrashamTrivedi@users.noreply.github.com>
2023-04-05 09:24:06 +0000

    Reverted a change that breaking applies and added some failing test cases back

--- cmd/data_test.go
+++ cmd/data_test.go
@@ -1,10 +1,12 @@
 package cmd
 
 import (
+	"strings"
 	"testing"
 )
 
 func TestGetProviderByName(t *testing.T) {
+
 	providerName, index := GetProviderByName("Github")
 
 	want := GitProvider{
@@ -21,6 +23,7 @@ func TestGetProviderByName(t *testing.T) {
 }
 
 func TestGetProviderByUrl(t *testing.T) {
+	// ResetProviders()
 
 	// Test Case 1: Test with empty providerUrl, should return -1 and nil
 	provider, index := GetProviderByUrl("")
@@ -50,14 +53,45 @@ func TestGetProviderByUrl(t *testing.T) {
 		t.Errorf("Expected provider to be non-nil, but got %v", provider)
 	}
 	if provider.Name != "github" {
-		t.Errorf("Expected provider name to be GitHub, but got %v", provider.Name)
+		t.Errorf("Expected provider name to be Github, but got %v", provider.Name)
 	}
 
-	provider, index = GetProviderByUrl("https://github.com/myoffice")
+	provider, index = GetProviderByUrl("https://github.com/myoffice/")
 	if index != -1 {
 		t.Errorf("Expected index to be -1, but got %v", index)
 	}
 	if provider.Name != "" {
 		t.Errorf("Expected provider to be nil, but got %v", provider)
 	}
+}
+
+func TestApply(t *testing.T) {
+	failingScenario := []GitProvider{
+		{
+			Name: "Github", Url: "https://github.com/",
+		},
+		{
+			Name: "GithubOffice", Url: "https://github.com/office/",
+		},
+	}
+
+	writeGitProviders(failingScenario)
+
+	provider, index := GetProviderByUrl("https://github.com/PrashamTrivedi/gitconfig-provider.git")
+
+	if index == -1 {
+		t.Errorf("Provider must be found")
+	}
+	if strings.ToLower(provider.Name) != "github" {
+		t.Errorf("Provider Name should be github but got %s", provider.Name)
+	}
+
+	provider, index = GetProviderByUrl("https://github.com/office/my-repo.git")
+	if index == -1 {
+		t.Errorf("Provider must be found")
+	}
+	if strings.ToLower(provider.Name) != "githuboffice" {
+		t.Errorf("Provider Name should be GithubOffice but got %s", provider.Name)
+	}
+	ResetProviders()
 }
