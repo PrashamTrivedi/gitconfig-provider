55c4e2c5ca84aa096bf98b7ec3f684206a03159a
PrashamTrivedi <PrashamTrivedi@users.noreply.github.com>
2023-04-10 06:12:21 +0000

    Feature: Add a provider forcefully (Experimental)

--- cmd/addProvider.go
+++ cmd/addProvider.go
@@ -1,11 +1,14 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
 //create two string variables to hold providerName and providerURL
 var providerName, providerURL string
+var doForcefully bool
 
 //create cobra command
 var addProviderCmd = &cobra.Command{
@@ -14,13 +17,19 @@ var addProviderCmd = &cobra.Command{
 	Long:  `This command will add a git provider for you`,
 	Run: func(cmd *cobra.Command, args []string) {
 		//call add provider with the providerName and providerURL keep properties blank
-		AddProvider(providerName, providerURL)
+		provider, index := AddProvider(providerName, providerURL,doForcefully)
+		if index == -1 {
+			fmt.Println("Provider", provider.Name, "added successfully")
+		} else {
+			fmt.Println("Recognized existing provider with:", provider.Name, "& url:", provider.Url)
+		}
 	},
 }
 
 func init() {
 	addProviderCmd.Flags().StringVarP(&providerName, "name", "n", "", "Name of the provider")
 	addProviderCmd.Flags().StringVarP(&providerURL, "url", "u", "", "URL of the provider")
+	addProviderCmd.Flags().BoolVarP(&doForcefully, "force", "f", false, "HIGHLY EXPERIMENTAL: Add the provider forcefully")
 
 	RootCmd.AddCommand(addProviderCmd)
 }
